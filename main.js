f182b41872c84bf2b4d2db3621d0bd8a0aa764f2
lmarcin84 <lmarcin84@gmail.com>
2019-06-26 14:00:35 +0200

    Initial commit

--- main.js
+++ main.js
@@ -10,12 +10,12 @@ function convert() {
 
   var filterText = new RegExp("[^" + keepText + "]+", "gmu");
   //console.log(filter);
-  var i = 1;
-  while ((textIn.length) > i) {
-    i++;
-    textOut = textOut.trim();
-    textOut = textOut.replace(filterText, divider);
-    document.getElementById("textOut").value = textOut.split(divider).filter(function (x, n, s) { return s.indexOf(x) == n }).join(divider);
-  }
+  // var i = 1;
+  // while ((textIn.length) > i) {
+    // i++;
+  textOut = textOut.trim();
+  textOut = textOut.replace(filterText, divider);
+  document.getElementById("textOut").value = textOut.split(divider).filter(function (x, n, s) { return s.indexOf(x) == n }).join(divider);
+  // }
 }
 

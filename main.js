8ec6587c5af4a3fba28221f697231d6352af20a4
lmarcin84 <lmarcin84@gmail.com>
2019-06-26 14:22:42 +0200

    Initial commit

--- main.js
+++ main.js
@@ -1,21 +1,12 @@
 // while
 function convert() {
-  var textIn = document.getElementById("textIn").value;
-  var textOut = document.getElementById("textOut").value; 
-  var divider = document.getElementById("divider").value;
-  var keepText = document.getElementById("keepText").value;
-  
-  if (!textOut) textOut = textIn 
-  else textOut = "";
-
-  var filterText = new RegExp("[^" + keepText + "]+", "gmu");
-  //console.log(filter);
-  // var i = 1;
-  // while ((textIn.length) > i) {
-    // i++;
-  textOut = textOut.trim();
-  textOut = textOut.replace(filterText, divider);
-  document.getElementById("textOut").value = textOut.split(divider).filter(function (x, n, s) { return s.indexOf(x) == n }).join(divider);
-  // }
+      var textIn = document.getElementById("textIn").value;
+      var textOut = document.getElementById("textOut").value; 
+      var divider = document.getElementById("divider").value;
+      var keepText = document.getElementById("keepText").value;
+      var filterText = new RegExp("[^" + keepText + "]+", "gmu");
+      (!textOut) ? textOut = textIn : textOut = "";
+      textOut = textOut.trim().replace(filterText, divider);
+      document.getElementById("textOut").value = textOut.split(divider).filter(function (x, n, s) { return s.indexOf(x) == n }).join(divider);
 }
 

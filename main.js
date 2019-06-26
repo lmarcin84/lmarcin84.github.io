ea53eb97eaa5549235bf11f9ad7caa6a75841de6
lmarcin84 <lmarcin84@gmail.com>
2019-06-26 13:15:52 +0200

    Initial commit

--- main.js
+++ main.js
@@ -1,29 +1,21 @@
-// for
-// function przepisz() {
-//   if (!document.form1.pole2.value) document.form1.pole2.value = document.form1.pole1.value
-//   else document.form1.pole2.value = ''
-
-//   for (i = 1; i < document.form1.pole1.value.length - 1; i++) {
-//     document.form1.pole2.value = document.form1.pole2.value.trim();
-//     var filter = new RegExp("[^" + document.getElementById('filtr').value + "]+", "gmu");
-//     console.log(filter);
-//     document.form1.pole2.value = document.form1.pole2.value.replace(filter, document.form1.pole3.value);
-//     document.form1.pole2.value = document.form1.pole2.value.split(document.form1.pole3.value).filter(function (x, n, s) { return s.indexOf(x) == n }).join(document.form1.pole3.value);
-//   }
-// }
-
 // while
-function przepisz() {
-  if (!document.form1.pole2.value) document.form1.pole2.value = document.form1.pole1.value
-  else document.form1.pole2.value = '';
-  var filter = new RegExp("[^" + document.getElementById('filtr').value + "]+", "gmu");
+function convert() {
+  var textIn = document.getElementById("textIn").value;
+  var textOut = document.getElementById("textOut").value; 
+  var divider = document.getElementById("divider").value;
+  var keepText = document.getElementById("keepText").value;
+  
+  if (!textOut) textOut = textIn 
+  else textOut = "";
+
+  var filterText = new RegExp("[^" + keepText + "]+", "gmu");
   //console.log(filter);
   var i = 1;
-  while ((document.form1.pole1.value.length) > i) {
+  while ((textIn.length) > i) {
     i++;
-    document.form1.pole2.value = document.form1.pole2.value.trim();
-    document.form1.pole2.value = document.form1.pole2.value.replace(filter, document.form1.pole3.value);
-    document.form1.pole2.value = document.form1.pole2.value.split(document.form1.pole3.value).filter(function (x, n, s) { return s.indexOf(x) == n }).join(document.form1.pole3.value);
+    textOut = textOut.trim();
+    textOut = textOut.replace(filterText, divider);
+    document.getElementById("textOut").value = textOut.split(divider).filter(function (x, n, s) { return s.indexOf(x) == n }).join(divider);
   }
 }
 
